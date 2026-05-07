5548ae6d7f925c9281b39b2434603484997d23b7
the-coder-tarun <km.taruneshwar@gmail.com>
2026-05-07 18:06:27 +0530

    fix(security): Missing Input Validation
    Fixes CWE-20 in models/user.js

--- models/user.js
+++ models/user.js
@@ -10,23 +10,38 @@ module.exports = function (sequelize, DataTypes) {
         name: {
             type: DataTypes.STRING,
             allowNull: false,
+            validate: {
+                is: /^[a-zA-Z0-9\s]+$/
+            }
         },
         login: {
             type: DataTypes.STRING,
             allowNull: false,
-            unique: true
+            unique: true,
+            validate: {
+                is: /^[a-zA-Z0-9]+$/
+            }
         },
         email: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
+            validate: {
+                isEmail: true
+            }
         },
         password: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
+            validate: {
+                len: [8, 100]
+            }
         },
         role: {
             type: DataTypes.STRING,
-            allowNull: true
+            allowNull: true,
+            validate: {
+                isIn: [["admin", "user"]]
+            }
         }
     });
     return User;
